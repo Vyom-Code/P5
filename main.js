ac3a3b90b1fea6b4d3ed8286bb33ab83b322ff3d
Vyom-Code <82333325+Vyom-Code@users.noreply.github.com>
2022-02-26 09:24:43 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+   image1=loadImage('https://www.gardendesign.com/pictures/images/900x705Max/dream-team-s-portland-garden_6/marigold-flowers-orange-pixabay_12708.jpg');
+   
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    
+}
+
+function draw(){
+    //image(image1, 0, 0, 400, 300);
+    fill("green");
+    stroke(0, 128,0);
+    circle(350, 50, 70);
+    circle(350, 50, 70);
+    circle(350, 50, 70);
+    circle(350, 50, 70);
+    fill("red");
+    stroke(0, 128,0);
+    rect(70, 50, 200, 50 )
+    rect(70, 50, 200, 50 )
+    rect(70, 50, 200, 50 )
+    rect(70, 50, 200, 50 )
+}
+
